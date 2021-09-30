ae258cf3a1f2b8ae228ca23f04349c834898ad30
Jeremy <jeremyfifty9@gmail.com>
2021-09-30 12:45:18 -0500

    processing for additional data subsets

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -6,7 +6,9 @@ from sklearn.feature_selection import VarianceThreshold
 from rdkit import Chem
 from rdkit.Chem import Descriptors
 
+
 from helpers import count_cf_bonds, create_morgan_space
+from graphnn import mol2graph
 
 def generate_dataset(splitter, name):
     ldtoxdb = pd.read_csv('../data/ldtoxdb-mordred.csv').dropna(axis=1)
@@ -16,7 +18,13 @@ def generate_dataset(splitter, name):
     ldtoxdb['mol_wt'] = ldtoxdb.rd_mol.apply(Chem.Descriptors.MolWt)
     ldtoxdb['is_pfas_like'] = ldtoxdb['n_cf_bonds'] >= 2
 
+    pfas8k = pd.read_csv('../data/pfas8k-mordred.csv')
+    pfas8k['canon_smi'] = pfas8k.SMILES.apply(Chem.MolFromSmiles).apply(Chem.MolToSmiles)
+    ldtoxdb['is_pfas'] = ldtoxdb.SMI.isin(pfas8k.canon_smi)
+
     mordred = ldtoxdb.columns[5:-5]
+    
+    # comment next 4 lines for speed if processing data only for non-benchmarks
     ecfp4096 = np.array(ldtoxdb.rd_mol.apply(create_morgan_space(nbits=4096, r=2)).tolist())
     ecfp2048 = np.array(ldtoxdb.rd_mol.apply(create_morgan_space(nbits=2048, r=1)).tolist())
     ecfp2048r6 = np.array(ldtoxdb.rd_mol.apply(create_morgan_space(nbits=2048, r=6)).tolist())
@@ -32,58 +40,103 @@ def generate_dataset(splitter, name):
         train = ldtoxdb.iloc[train_idx]
         test = ldtoxdb.iloc[test_idx]
 
-        pfas_train = train.loc[train.is_pfas_like]
-        pfas_test = test.loc[test.is_pfas_like]
+        pfas_like_pfas_excluded_train = train.loc[(train.is_pfas_like & ~train.is_pfas)]
+        pfas_like_pfas_excluded_test = test.loc[(test.is_pfas_like & ~test.is_pfas)]
+        pfas_like_train = train.loc[train.is_pfas_like]
+        pfas_like_test = test.loc[test.is_pfas_like]
+        pfas_like_excluded_train = train.loc[~train.is_pfas_like]
+        pfas_like_excluded_test = test.loc[~test.is_pfas_like]
+        pfas_excluded_train = train.loc[~train.is_pfas]
+        pfas_excluded_test = test.loc[~test.is_pfas]
+        pfas_train = train.loc[train.is_pfas]
+        pfas_test = test.loc[test.is_pfas]
 
-        pfas_train_idx = pfas_train.index
-        pfas_test_idx = pfas_test.index
+        pfas_like_train_idx = pfas_like_train.index
+        pfas_like_test_idx = pfas_like_test.index
 
         # SMILES
         np.savez_compressed(prefix + '_smiles_test', smiles=test[['SMI']].to_numpy())
         np.savez_compressed(prefix + '_smiles_train', smiles=train[['SMI']].to_numpy())
 
-        np.savez_compressed(prefix + '_smiles_test_pfas_like', smiles=pfas_test[['SMI']].to_numpy())
-        np.savez_compressed(prefix + '_smiles_train_pfas_like', smiles=pfas_train[['SMI']].to_numpy())
+        np.savez_compressed(prefix + '_smiles_test_pfas_like', smiles=pfas_like_test[['SMI']].to_numpy())
+        np.savez_compressed(prefix + '_smiles_train_pfas_like', smiles=pfas_like_train[['SMI']].to_numpy())
+
+        np.savez_compressed(prefix + '_smiles_test_pfas_like_excluded', smiles=pfas_like_excluded_test[['SMI']].to_numpy())
+        np.savez_compressed(prefix + '_smiles_train_pfas_like_excluded', smiles=pfas_like_excluded_train[['SMI']].to_numpy())
+
+        np.savez_compressed(prefix + '_smiles_test_pfas_like_pfas_excluded', smiles=pfas_like_pfas_excluded_test[['SMI']].to_numpy())
+        np.savez_compressed(prefix + '_smiles_train_pfas_like_pfas_excluded', smiles=pfas_like_pfas_excluded_train[['SMI']].to_numpy())
+
+        np.savez_compressed(prefix + '_smiles_test_pfas_excluded', smiles=pfas_excluded_test[['SMI']].to_numpy())
+        np.savez_compressed(prefix + '_smiles_train_pfas_excluded', smiles=pfas_excluded_train[['SMI']].to_numpy())
+
+        np.savez_compressed(prefix + '_smiles_test_pfas', smiles=pfas_test[['SMI']].to_numpy())
+        np.savez_compressed(prefix + '_smiles_train_pfas', smiles=pfas_train[['SMI']].to_numpy())
 
         # Outputs
         np.savez_compressed(prefix + '_y_test', y=test[['NeglogLD50']].to_numpy())
         np.savez_compressed(prefix + '_y_train', y=train[['NeglogLD50']].to_numpy())
 
-        np.savez_compressed(prefix + '_y_test_pfas_like', y=pfas_test[['NeglogLD50']].to_numpy())
-        np.savez_compressed(prefix + '_y_train_pfas_like', y=pfas_train[['NeglogLD50']].to_numpy())
+        np.savez_compressed(prefix + '_y_test_pfas_like', y=pfas_like_test[['NeglogLD50']].to_numpy())
+        np.savez_compressed(prefix + '_y_train_pfas_like', y=pfas_like_train[['NeglogLD50']].to_numpy())
+
+        np.savez_compressed(prefix + '_y_test_pfas_like_excluded', y=pfas_like_excluded_test[['NeglogLD50']].to_numpy())
+        np.savez_compressed(prefix + '_y_train_pfas_like_excluded', y=pfas_like_excluded_train[['NeglogLD50']].to_numpy())
+
+        np.savez_compressed(prefix + '_y_test_pfas_like_pfas_excluded', y=pfas_like_pfas_excluded_test[['NeglogLD50']].to_numpy())
+        np.savez_compressed(prefix + '_y_train_pfas_like_pfas_excluded', y=pfas_like_pfas_excluded_train[['NeglogLD50']].to_numpy())
+
+        np.savez_compressed(prefix + '_y_test_pfas_excluded', y=pfas_excluded_test[['NeglogLD50']].to_numpy())
+        np.savez_compressed(prefix + '_y_train_pfas_excluded', y=pfas_excluded_train[['NeglogLD50']].to_numpy())
+
+        np.savez_compressed(prefix + '_y_test_pfas', y=pfas_test[['NeglogLD50']].to_numpy())
+        np.savez_compressed(prefix + '_y_train_pfas', y=pfas_train[['NeglogLD50']].to_numpy())
 
         # Mordred inputs
         col_selector = VarianceThreshold()
         np.savez_compressed(prefix + '_mordred_x_train', x=col_selector.fit_transform(train[mordred]).astype(np.float32))
         np.savez_compressed(prefix + '_mordred_x_test', x=col_selector.transform(test[mordred]).astype(np.float32))
 
-        np.savez_compressed(prefix + '_mordred_x_train_pfas_like', x=col_selector.transform(pfas_train[mordred]).astype(np.float32))
-        np.savez_compressed(prefix + '_mordred_x_test_pfas_like', x=col_selector.transform(pfas_test[mordred]).astype(np.float32))
+        np.savez_compressed(prefix + '_mordred_x_train_pfas_like', x=col_selector.transform(pfas_like_train[mordred]).astype(np.float32))
+        np.savez_compressed(prefix + '_mordred_x_test_pfas_like', x=col_selector.transform(pfas_like_test[mordred]).astype(np.float32))
+
+        np.savez_compressed(prefix + '_mordred_x_train_pfas_like_excluded', x=col_selector.transform(pfas_like_excluded_train[mordred]).astype(np.float32))
+        np.savez_compressed(prefix + '_mordred_x_test_pfas_like_excluded', x=col_selector.transform(pfas_like_excluded_test[mordred]).astype(np.float32))
+
+        np.savez_compressed(prefix + '_mordred_x_train_pfas_like_pfas_excluded', x=col_selector.transform(pfas_like_pfas_excluded_train[mordred]).astype(np.float32))
+        np.savez_compressed(prefix + '_mordred_x_test_pfas_like_pfas_excluded', x=col_selector.transform(pfas_like_pfas_excluded_test[mordred]).astype(np.float32))
+
+        np.savez_compressed(prefix + '_mordred_x_train_pfas_excluded', x=col_selector.transform(pfas_excluded_train[mordred]).astype(np.float32))
+        np.savez_compressed(prefix + '_mordred_x_test_pfas_excluded', x=col_selector.transform(pfas_excluded_test[mordred]).astype(np.float32))
+
+        np.savez_compressed(prefix + '_mordred_x_train_pfas', x=col_selector.transform(pfas_train[mordred]).astype(np.float32))
+        np.savez_compressed(prefix + '_mordred_x_test_pfas', x=col_selector.transform(pfas_test[mordred]).astype(np.float32))
 
         # We need these for inference later on
         indices = col_selector.get_support(indices=True)
         np.savez_compressed(prefix + '_mordred_x_cols', cols=train[mordred].iloc[[0], indices].columns)
+        continue
 
         # ECFP-4096 inputs
         np.savez_compressed(prefix + '_ecfp_4096_x_train', x=col_selector.fit_transform(ecfp4096[train_idx]).astype(np.float32))
         np.savez_compressed(prefix + '_ecfp_4096_x_test', x=col_selector.transform(ecfp4096[test_idx]).astype(np.float32))
 
-        np.savez_compressed(prefix + '_ecfp_4096_x_train_pfas_like', x=col_selector.transform(ecfp4096[pfas_train_idx]).astype(np.float32))
-        np.savez_compressed(prefix + '_ecfp_4096_x_test_pfas_like', x=col_selector.transform(ecfp4096[pfas_test_idx]).astype(np.float32))
+        np.savez_compressed(prefix + '_ecfp_4096_x_train_pfas_like', x=col_selector.transform(ecfp4096[pfas_like_train_idx]).astype(np.float32))
+        np.savez_compressed(prefix + '_ecfp_4096_x_test_pfas_like', x=col_selector.transform(ecfp4096[pfas_like_test_idx]).astype(np.float32))
 
         # ECFP-2048 inputs
         np.savez_compressed(prefix + '_ecfp_2048_x_train', x=col_selector.fit_transform(ecfp2048[train_idx]).astype(np.float32))
         np.savez_compressed(prefix + '_ecfp_2048_x_test', x=col_selector.transform(ecfp2048[test_idx]).astype(np.float32))
 
-        np.savez_compressed(prefix + '_ecfp_2048_x_train_pfas_like', x=col_selector.transform(ecfp2048[pfas_train_idx]).astype(np.float32))
-        np.savez_compressed(prefix + '_ecfp_2048_x_test_pfas_like', x=col_selector.transform(ecfp2048[pfas_test_idx]).astype(np.float32))
+        np.savez_compressed(prefix + '_ecfp_2048_x_train_pfas_like', x=col_selector.transform(ecfp2048[pfas_like_train_idx]).astype(np.float32))
+        np.savez_compressed(prefix + '_ecfp_2048_x_test_pfas_like', x=col_selector.transform(ecfp2048[pfas_like_test_idx]).astype(np.float32))
 
         # ECFP-2048 inputs
-        np.savez_compressed(prefix + '_ecfp_2048r6_x_train', x=col_selector2.fit_transform(ecfp2048r6[train_idx]).astype(np.float32))
+        np.savez_compressed(prefix + '_ecfp_2048r6_x_train', x=col_selector.fit_transform(ecfp2048r6[train_idx]).astype(np.float32))
         np.savez_compressed(prefix + '_ecfp_2048r6_x_test', x=col_selector.transform(ecfp2048r6[test_idx]).astype(np.float32))
 
-        np.savez_compressed(prefix + '_ecfp_2048r6_x_train_pfas_like', x=col_selector.transform(ecfp2048r6[pfas_train_idx]).astype(np.float32))
-        np.savez_compressed(prefix + '_ecfp_2048r6_x_test_pfas_like', x=col_selector.transform(ecfp2048r6[pfas_test_idx]).astype(np.float32))
+        np.savez_compressed(prefix + '_ecfp_2048r6_x_train_pfas_like', x=col_selector.transform(ecfp2048r6[pfas_like_train_idx]).astype(np.float32))
+        np.savez_compressed(prefix + '_ecfp_2048r6_x_test_pfas_like', x=col_selector.transform(ecfp2048r6[pfas_like_test_idx]).astype(np.float32))
 
         # GP Convienience
         col_selector2 = VarianceThreshold()
